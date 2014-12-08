dd55f25a0da3c860594c121a788c16400b8c12df
mhejmadi <mhejmadi@wellesley.edu>
2014-12-07 23:19:34 -0500

    Created AllStats.

---
+++ AllStats.java
@@ -0,0 +1,87 @@
+/* Meera Hejmadi and Pamela Wang
+ * Final Project - Battleship
+ * AllStats.java
+ * Dec 7th, 2014
+ * 
+ * Purpose: To create a LinkedList of all users' stats.
+ */
+
+import java.util.*;
+
+public class AllStats {
+ 
+  private LinkedList<Stats> gameStats;
+  private int numUsers;
+  private final int NOT_FOUND = -1;
+  
+  public AllStats() {
+    gameStats = new LinkedList<Stats>();
+    numUsers = 0;
+  }
+  
+  public int getPlayerIndex(String username) {
+    LinkedList<Stats> temp = new LinkedList<Stats>();
+    System.out.println("getPlayerIndex(): temp is: " + temp);
+    int index = NOT_FOUND;
+    boolean found = false;
+    while (!gameStats.isEmpty()) {
+      System.out.println("getPlayerIndex(): gameStats isn't empty.");
+      temp.add(gameStats.remove());
+      System.out.println("getPlayerIndex(): temp is: " + temp);
+      if (temp.getFirst().getUserName().equals(username)) {
+        System.out.println("getPlayerIndex(): It's a match!");
+        index++; //because we start at -1
+        found = true;
+        break;
+      } else {
+        System.out.println("getPlayerIndex(): Still haven't found it.");
+        index++;
+      }
+    }
+    index = (found == true) ? index : NOT_FOUND; 
+    System.out.println("getPlayerIndex(): Index: " + index);
+    gameStats = temp;
+    return index;
+  }
+  
+  public void addPlayer(String username) {
+    System.out.println("addPlayer(): trying to add " + username);
+    if (getPlayerIndex(username) == NOT_FOUND) {
+      System.out.println("addPlayer(): adding " + username + " now.");
+      Stats current = new Stats(username);
+      this.gameStats.add(current);
+      numUsers++;
+      System.out.println("addPlayer(): gameStats: " + gameStats);
+      System.out.println("addPlayer(): Should be " + numUsers + " entries.");
+    }
+  }
+  
+  public String toString() {
+    LinkedList<Stats> temp = new LinkedList<Stats>();
+    String s = "Users who have played so far: ";
+    int i = 0;
+    while (!gameStats.isEmpty()) {
+      temp.add(gameStats.remove());
+      System.out.println("toString(): temp is: " + temp);
+      System.out.println("toString(): gameStats is: " + gameStats);
+      s += temp.get(i).getUserName() + " ";
+      System.out.println("toString(): Got first username.");
+      i++;
+    }
+    gameStats = temp;
+    System.out.println("toString(): Re-assigned gameStats.\tgameStats: " + gameStats);
+    return s;
+  }
+    
+  public static void main (String[] args) {
+    AllStats game1 = new AllStats();
+    System.out.println(game1);
+    game1.addPlayer("mhejmadi");
+    System.out.println(game1);
+    game1.addPlayer("pwang2");
+    System.out.println(game1);
+    
+  }
+    
+  
+}
