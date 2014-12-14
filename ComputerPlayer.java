f3592dd49af7f095bd879e03ad8e5abc754d156d
mhejmadi <mhejmadi@wellesley.edu>
2014-12-14 15:03:04 -0500

    Almost fixed the overlapping. Check note at the top and doesBoatOverlap()

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -5,6 +5,10 @@
  * 
  * Purpose: 
  * 
+ * NOTES:
+ * 1. Need to fix the checking for a boat (doesBoatOverlap()) - have to start from the
+ * least between start and end.
+ * 
  * @author Meera Hejmadi
  * @author Pamela Wang
  */
@@ -74,201 +78,225 @@ public class ComputerPlayer extends Player {
       int direction = (boatOrientation == VERTICAL) ? getUpOrDown() : getLeftOrRight();
       Boat blackPearl = fleet.get(i);
       setCoords(blackPearl, direction); //makes and checks Boat coordinates
+      
       if (direction == UP || direction == DOWN) {
+        System.out.println("placeBoats(): Boat's upward/downward. ChangingCells.");
         for (int j = blackPearl.getStartY(); j <= blackPearl.getEndY(); j++) {
           grid[blackPearl.getStartX()][j-1].setHasBoat(true);
         }
       } else {
-          for (int j = blackPearl.getStartX(); j <= blackPearl.getEndX(); j++) {
-            grid[j-1][blackPearl.getStartY()].setHasBoat(true);
-          }
+        System.out.println("placeBoats(): Boat's left/right. ChangingCells.");
+        for (int j = blackPearl.getStartX(); j <= blackPearl.getEndX(); j++) {
+          grid[j-1][blackPearl.getStartY()].setHasBoat(true);
         }
-        System.out.println("Boat " + Integer.toString(i+1) + "\n" + blackPearl + "\n");
       }
-      
-      System.out.println(findMyFleet());
+      System.out.println("Boat " + Integer.toString(i+1) + "\n" + blackPearl + "\n");
+      System.out.println("placeBoats(): GRID PRINT OUT" + printGrid());
     }
     
-    //helpers for PlaceBoats
-    private void setCoords(Boat b, int direction) {
-      //THEORETICALLY CAN STILL USE PICKAPOINT()
+    System.out.println(findMyFleet());
+  }
+  
+  //helpers for PlaceBoats
+  private void setCoords(Boat b, int direction) {
+    //THEORETICALLY CAN STILL USE PICKAPOINT()
 //      System.out.println("setCoords(): X");
-      b.setStartX(makeValidCoord((int)(Math.random()*10))); //using Boat's getters and setters as 'local' variables
+    b.setStartX(makeValidCoord((int)(Math.random()*10))); //using Boat's getters and setters as 'local' variables
 //      System.out.println("setCoords(): Y");
-      b.setStartY(makeValidCoord((int)(Math.random()*10)));
-      
-      System.out.println("setCoords(): Already a boat? " + grid[b.getStartX()-1][b.getStartY()-1].getHasBoat());
-      if (grid[b.getStartX()-1][b.getStartY()-1].getHasBoat()) { //no boat
-        System.out.println("setCoords(): checking for previous boat. Found one.");
-        setCoords(b, direction); //find new coordinates
-      } else {
-        setEndCoords(b, direction);
-      }
-      
-      
-      
-      /*OLD CODE BELOW
-       if (foundEndCoords == false) {
-       switch (direction) {
-       case 2: //UP, vertical orientation (startX == endX)
-       //Java or DrJava doesn't allow for the use of constants in switch statements, so we're hard coding here
-       b.setEndX(b.getStartX()); //same x-coordinate
-       b.setEndY(setEndCoords(b, direction, 1)); //here with Meera
-       foundEndCoords = true;
-       break;
-       
-       case 3: //DOWN, vertical orientation
-       b.setEndX(b.getStartX());
-       b.setEndY(setEndCoords(b, direction, 1));
-       foundEndCoords = true;
-       break;
-       
-       case 4: //LEFT, horizontal (startY == endY)
-       b.setEndY(b.getEndY()); //same y-coordinate
-       b.setEndX(setEndCoords(b, direction, 1));
-       foundEndCoords = true;
-       break;
-       
-       case 5: //RIGHT, horizontal
-       b.setEndY(b.getEndY());
-       b.setEndX(setEndCoords(b, direction, 1));
-       foundEndCoords = true;
-       break;
-       
-       default:
-       break;
-       }
-       } else { //foundEndCoords == true
-       return;
-       }*/
+    b.setStartY(makeValidCoord((int)(Math.random()*10)));
+    
+    System.out.println("setCoords(): Already a boat? " + grid[b.getStartX()-1][b.getStartY()-1].getHasBoat());
+    if (grid[b.getStartX()-1][b.getStartY()-1].getHasBoat()) { //no boat
+      System.out.println("setCoords(): checking for previous boat. Found one.");
+      setCoords(b, direction); //find new coordinates
+    } else {
+      setEndCoords(b, direction);
     }
     
     
-    private void setEndCoords(Boat ship, int direction) { //recursive
-      int startX = ship.getStartX();
-      int startY = ship.getStartY();
-      int length = ship.getLength();
-      boolean foundEndCoords = false;
-      int directionsTried = 0;
-      
-      while (directionsTried <= 4 && !foundEndCoords) {
+    
+    /*OLD CODE BELOW
+     if (foundEndCoords == false) {
+     switch (direction) {
+     case 2: //UP, vertical orientation (startX == endX)
+     //Java or DrJava doesn't allow for the use of constants in switch statements, so we're hard coding here
+     b.setEndX(b.getStartX()); //same x-coordinate
+     b.setEndY(setEndCoords(b, direction, 1)); //here with Meera
+     foundEndCoords = true;
+     break;
+     
+     case 3: //DOWN, vertical orientation
+     b.setEndX(b.getStartX());
+     b.setEndY(setEndCoords(b, direction, 1));
+     foundEndCoords = true;
+     break;
+     
+     case 4: //LEFT, horizontal (startY == endY)
+     b.setEndY(b.getEndY()); //same y-coordinate
+     b.setEndX(setEndCoords(b, direction, 1));
+     foundEndCoords = true;
+     break;
+     
+     case 5: //RIGHT, horizontal
+     b.setEndY(b.getEndY());
+     b.setEndX(setEndCoords(b, direction, 1));
+     foundEndCoords = true;
+     break;
+     
+     default:
+     break;
+     }
+     } else { //foundEndCoords == true
+     return;
+     }*/
+  }
+  
+  
+  private void setEndCoords(Boat ship, int direction) { //recursive
+    int startX = ship.getStartX();
+    int startY = ship.getStartY();
+    int length = ship.getLength();
+    boolean foundEndCoords = false;
+    int directionsTried = 0;
+    
+    while (directionsTried <= 4 && !foundEndCoords) {
 //        System.out.println("setEndCoords(): directionsTried: " + directionsTried
 //                             + " Haven't found the endCoords.");
-        if (direction == UP) { //direction
-          System.out.println("setEndCoords(): Direction = UP");
-          directionsTried++;
-          ship.setEndX(startX);
-          ship.setEndY(startY - (length-1));
+      if (direction == UP) { //direction
+        System.out.println("setEndCoords(): Direction = UP");
+        directionsTried++;
+        ship.setEndX(startX);
+        ship.setEndY(startY - (length-1));
 //          System.out.println("setEndCoords(): endY = "+ (startY - (length-1)));
-          if (isCoordValid(ship.getEndY())) {
-            System.out.println("setEndCoords(): endY (" + ship.getEndY() + ") is valid");
-            foundEndCoords = true;
-          } else {
-            System.out.println("setEndCoords(): endY (" + ship.getEndY() + ")is not valid. Direction++");
-            direction++;
-          }
-        } else if (direction == DOWN) { //direction == 3
-          System.out.println("setEndCoords(): Direction = DOWN");
-          directionsTried++;
-          ship.setEndX(startX);
-          ship.setEndY(startY + (length-1));
+        if (isCoordValid(ship.getEndY()) && !doesBoatOverlap(ship, direction)) {
+          System.out.println("setEndCoords(): endY (" + ship.getEndY() + ") is valid");
+          foundEndCoords = true;
+        } else {
+          System.out.println("setEndCoords(): endY (" + ship.getEndY() + ")is not valid. Direction++");
+          direction++;
+        }
+      } else if (direction == DOWN) { //direction == 3
+        System.out.println("setEndCoords(): Direction = DOWN");
+        directionsTried++;
+        ship.setEndX(startX);
+        ship.setEndY(startY + (length-1));
 //          System.out.println("setEndCoords(): endY = "+ (startY + (length-1)));
-          if (isCoordValid(ship.getEndY())) {
-            System.out.println("setEndCoords(): endY (" + ship.getEndY() + ") is valid");
-            foundEndCoords = true;
-          } else {
-            System.out.println("setEndCoords(): endY (" + ship.getEndY() + ") is not valid. Direction++");
-            direction++;
-          }
-        } else if (direction == LEFT) { //direction == 4
-          System.out.println("setEndCoords(): Direction = LEFT");
-          directionsTried++;
-          ship.setEndX(startX - (length-1));
+        if (isCoordValid(ship.getEndY()) && !doesBoatOverlap(ship, direction)) {
+          System.out.println("setEndCoords(): endY (" + ship.getEndY() + ") is valid");
+          foundEndCoords = true;
+        } else {
+          System.out.println("setEndCoords(): endY (" + ship.getEndY() + ") is not valid. Direction++");
+          direction++;
+        }
+      } else if (direction == LEFT) { //direction == 4
+        System.out.println("setEndCoords(): Direction = LEFT");
+        directionsTried++;
+        ship.setEndX(startX - (length-1));
 //          System.out.println("setEndCoords(): endX = "+ (startX - (length-1)));
-          ship.setEndY(startY);
-          if (isCoordValid(ship.getEndX())) {
-            System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is valid");
-            foundEndCoords = true;
-          } else {
-            System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is not valid. Direction++");
-            direction++;
-          }
-        } else { //direction == RIGHT (5)
-          System.out.println("setEndCoords(): Direction = RIGHT");
-          directionsTried++;
-          ship.setEndX(startX + (length-1));
+        ship.setEndY(startY);
+        if (isCoordValid(ship.getEndX()) && !doesBoatOverlap(ship, direction)) {
+          System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is valid");
+          foundEndCoords = true;
+        } else {
+          System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is not valid. Direction++");
+          direction++;
+        }
+      } else { //direction == RIGHT (5)
+        System.out.println("setEndCoords(): Direction = RIGHT");
+        directionsTried++;
+        ship.setEndX(startX + (length-1));
 //          System.out.println("setEndCoords(): endX = "+ (startX + (length-1)));
-          ship.setEndY(startY);
-          if (isCoordValid(ship.getEndX())) {
-            System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is valid");
-            foundEndCoords = true;
-          } else {
-            System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is not valid. Direction++");
-            direction = UP; //restarting while loop
-          }
+        ship.setEndY(startY);
+        if (isCoordValid(ship.getEndX()) && !doesBoatOverlap(ship, direction)) {
+          System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is valid");
+          foundEndCoords = true;
+        } else {
+          System.out.println("setEndCoords(): endX (" + ship.getEndX() + ") is not valid. Direction++");
+          direction = UP; //restarting while loop
         }
       }
-      
-      if (!foundEndCoords) {
-        setCoords(ship, direction); //finding new starting coordinates
-      }
-      
-      /*OLD CODE
-       int coordinate = -1;
-       if (tries > 4) { //tried all directions
-       setCoords(ship, direction); //start anew and find new starting coordinates
-       } else if (tries > 2) { //tried both directions; tries is 
-       if (boatOrientation == VERTICAL) {
-       boatOrientation = HORIZONTAL;
-       setEndCoords(ship, getLeftOrRight(), tries);
-       } else { //boatOrientation == HORIZONTAL
-       boatOrientation = VERTICAL;
-       setEndCoords(ship, getUpOrDown(), tries);
-       }
-       }
-       
-       //
-       if (boatOrientation == VERTICAL) { //coordinate = endIndexY
-       if (direction == UP) {
-       coordinate = ship.getStartY() - (ship.getLength()-1);
-       coordinate = (coordinate < 1) ? setEndCoords(ship, DOWN, tries++) : coordinate;
-       } else if (direction == DOWN) {
-       coordinate = ship.getStartY() + (ship.getLength()-1);
-       coordinate = (coordinate > GRID_DIMENSIONS) ? setEndCoords(ship, UP, tries++) : coordinate;
-       }
-       } else { //boatOrientation == HORIZONTAL; coordinate = endIndexX
-       if (direction == LEFT) {
-       coordinate = ship.getStartX() - (ship.getLength()-1);
-       coordinate = (coordinate < 1) ? setEndCoords(ship, RIGHT, tries++) : coordinate;
-       } else { //direction == RIGHT
-       coordinate = ship.getStartX() + (ship.getLength()-1);
-       coordinate = (coordinate > GRID_DIMENSIONS) ? setEndCoords(ship, LEFT, tries++) : coordinate;
-       }
-       }
-       
-       return coordinate;*/
     }
     
-    private boolean isCoordValid (int coord) {
-      return (coord > 0  && coord <= GRID_DIMENSIONS);
+    if (!foundEndCoords) {
+      setCoords(ship, direction); //finding new starting coordinates
     }
     
-    private int getBoatOrientation() { //0 = vertical, 1 = horizontal
-      return (int)Math.round(Math.random());
-    }
-    
-    private int getUpOrDown() { //2 = up, down = 3
-      return (int)Math.round(Math.random() + 2);
-    }
-    
-    private int getLeftOrRight() { //4 = left, 5 = right
-      return (int)Math.round(Math.random() + 4);
-    }
-    
-    public static void main (String[] args) {
-      ComputerPlayer c = new ComputerPlayer();
-      System.out.println("findMyFleet(): " + c.findMyFleet());
-      c.placeBoats();
-    }
-    
+    /*OLD CODE
+     int coordinate = -1;
+     if (tries > 4) { //tried all directions
+     setCoords(ship, direction); //start anew and find new starting coordinates
+     } else if (tries > 2) { //tried both directions; tries is 
+     if (boatOrientation == VERTICAL) {
+     boatOrientation = HORIZONTAL;
+     setEndCoords(ship, getLeftOrRight(), tries);
+     } else { //boatOrientation == HORIZONTAL
+     boatOrientation = VERTICAL;
+     setEndCoords(ship, getUpOrDown(), tries);
+     }
+     }
+     
+     //
+     if (boatOrientation == VERTICAL) { //coordinate = endIndexY
+     if (direction == UP) {
+     coordinate = ship.getStartY() - (ship.getLength()-1);
+     coordinate = (coordinate < 1) ? setEndCoords(ship, DOWN, tries++) : coordinate;
+     } else if (direction == DOWN) {
+     coordinate = ship.getStartY() + (ship.getLength()-1);
+     coordinate = (coordinate > GRID_DIMENSIONS) ? setEndCoords(ship, UP, tries++) : coordinate;
+     }
+     } else { //boatOrientation == HORIZONTAL; coordinate = endIndexX
+     if (direction == LEFT) {
+     coordinate = ship.getStartX() - (ship.getLength()-1);
+     coordinate = (coordinate < 1) ? setEndCoords(ship, RIGHT, tries++) : coordinate;
+     } else { //direction == RIGHT
+     coordinate = ship.getStartX() + (ship.getLength()-1);
+     coordinate = (coordinate > GRID_DIMENSIONS) ? setEndCoords(ship, LEFT, tries++) : coordinate;
+     }
+     }
+     
+     return coordinate;*/
   }
+  
+  private boolean isCoordValid (int coord) {
+    return (coord > 0  && coord <= GRID_DIMENSIONS);
+  }
+  
+  private boolean doesBoatOverlap(Boat blackPearl, int direction) {
+    boolean overlap = false;
+    if (direction == UP || direction == DOWN) {
+      for (int j = blackPearl.getStartY(); j <= blackPearl.getEndY(); j++) {
+        System.out.println("doesBoatOverlap(): " + grid[blackPearl.getStartX()][j-1].getHasBoat());
+        overlap = (grid[blackPearl.getStartX()][j-1].getHasBoat()) ? true : overlap;
+        //if there's a boat, valid = false, else doesn't change
+        if (overlap) { return overlap; } //returns at first instance of boatOverlapping
+      }
+    } else {
+      for (int j = blackPearl.getStartX(); j <= blackPearl.getEndX(); j++) {
+        System.out.println("doesBoatOverlap(): " + grid[blackPearl.getStartX()][j-1].getHasBoat());
+        overlap = (grid[j-1][blackPearl.getStartY()].getHasBoat()) ? true : overlap;
+        //if there's a boat, valid = false, else doesn't change
+        if (overlap) { return overlap; } //returns at first instance of boatOverlapping
+      }
+    }
+    return overlap;
+  }
+  
+  private int getBoatOrientation() { //0 = vertical, 1 = horizontal
+    return (int)Math.round(Math.random());
+  }
+  
+  private int getUpOrDown() { //2 = up, down = 3
+    return (int)Math.round(Math.random() + 2);
+  }
+  
+  private int getLeftOrRight() { //4 = left, 5 = right
+    return (int)Math.round(Math.random() + 4);
+  }
+  
+  public static void main (String[] args) {
+    ComputerPlayer c = new ComputerPlayer();
+    System.out.println("findMyFleet(): " + c.findMyFleet());
+    c.placeBoats();
+  }
+  
+}
