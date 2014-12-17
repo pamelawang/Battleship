74ea646047b2e70fee25bd1cb9a66defe57b4374
pamelawang <pamela.n.wang@gmail.com>
2014-12-16 23:36:54 -0500

    Intro image

--- WelcomePanel.java
+++ WelcomePanel.java
@@ -10,35 +10,35 @@
 
 import java.awt.*;
 import javax.swing.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 public class WelcomePanel extends JPanel  {
   
   private JLabel title, instructions, authors, east, west;
   
   public WelcomePanel() {
-    
-    setBackground(Color.blue);
+    setBackground(Color.black);
     setLayout (new BorderLayout());
     
     title = new JLabel ("Battleship");
-    title.setHorizontalAlignment(SwingConstants.CENTER);
-    this.add(title, BorderLayout.NORTH);
-    
-    east = new JLabel(" "); //blank to use formatting rules of BorderLayout
-    this.add(east, BorderLayout.EAST);
-    
-    west = new JLabel(" "); //blank to use formatting rules of BorderLayout
-    this.add(west, BorderLayout.WEST); 
-    
-    instructions = new JLabel ("How to play");
-    instructions.setHorizontalAlignment(SwingConstants.CENTER);
-    this.add(instructions, BorderLayout.CENTER);
-    
-    authors = new JLabel ("\u00A9 2014 Meera Hejmadi and Pamela Wang");
-    authors.setHorizontalAlignment(SwingConstants.CENTER);
-    this.add(authors, BorderLayout.SOUTH);
-    
-    //this.add(new JLabel(new ImageIcon("IMAGE NAME HERE"));
+    File i = new File("intro.png");
+    try {
+      Image intro = ImageIO.read(i);
+      intro = getScaledImage(intro, 1000, 700);
+      this.add(new JLabel(new ImageIcon(intro)));
+    } catch (IOException e) {
+    }
   } 
+  
+  private Image getScaledImage(Image srcImg, int w, int h){
+    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = resizedImg.createGraphics();
+    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    g2.drawImage(srcImg, 0, 0, w, h, null);
+    g2.dispose();
+    return resizedImg;
+  }
  
 }//ends WelcomePanel
