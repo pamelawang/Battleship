5e300fcb47bb8e8de1fd48081f5a5d79a108c714
mhejmadi <mhejmadi@wellesley.edu>
2014-12-17 10:25:10 -0500

    Commented most of ComputerPlayer

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -19,7 +19,7 @@ public class ComputerPlayer extends Player {
   private GridButton nextShot;
   private int aimAtX;
   private int aimAtY;
-  private final int INVALID = -1; //make Player's protected later
+  private final int INVALID = -1;
   private Cell[][] shotSoFarGrid;
   private final int VERTICAL = 0;
   private final int HORIZONTAL = 1;
@@ -30,9 +30,12 @@ public class ComputerPlayer extends Player {
   private int boatOrientation; //used for placing boats
   private int direction; //direction we're shooting in after hitting something
   private boolean wasRandomShot; //if true, then the last shot taken was a random shot 
-  private int lastShotResult;
-  private int round = 0; //TO DELETE AFTER DEBUGGING
+  private int lastShotResult; //the last shot was a hit/miss/sunk
   
+  /**********************************************************************************
+    * Constructor: Creates a new ComputerPlayer, and sets instance variables to 
+    * default. 
+    *********************************************************************************/
   public ComputerPlayer() {
     super();
     aimAtX = INVALID;
@@ -48,7 +51,11 @@ public class ComputerPlayer extends Player {
   }
   
   
-  private void pickAPoint() { //randomiser
+  /**********************************************************************************
+    * Private. Randomly chooses the computer's next pair of coordinates to shoot at. 
+    * Sets aimAtX and aimAtY as these coordinates.
+    **********************************************************************************/
+private void pickAPoint() { //randomiser
     do {
       int randomX = (int) (Math.random()*10);
       aimAtX = makeValidCoord(randomX);
@@ -58,18 +65,36 @@ public class ComputerPlayer extends Player {
     } while (previouslyShotAt(aimAtX, aimAtY));
   }
   
-  private int makeValidCoord (int coordinate) { //checks if coordinate is within grid dimensions
+ /**********************************************************************************
+    * Private. Ensures the input coordinate is between 1 and GRID_DIMENSIONS.
+    * Returns a valid int.
+    * 
+    * @param    coordinate    int whose validity is to be checked.
+    * @return     int      int that is valid.
+    *********************************************************************************/
+ private int makeValidCoord (int coordinate) { //checks if coordinate is within grid dimensions
     int valid = (coordinate >= 1) ? coordinate : 1;
     valid = (valid <= GRID_DIMENSIONS) ? valid : GRID_DIMENSIONS;
     System.out.println("makeValidCoord: coordinate = " + coordinate + " and valid = " + valid);
     return valid;
   }
   
+   /********************************************************************************
+    * Private. Returns whether the input coordinate pair has already been shot at. Uses Cell.
+    * 
+    * @param    xCoord    the x-coordinate 
+    * @param    yCoord    the y-coordinate
+    * @return    boolean    has this coordinate pair been shot at before
+    *********************************************************************************/
   private boolean previouslyShotAt (int xCoord, int yCoord) {
     return shotSoFarGrid[xCoord-1][yCoord-1].getShotAt();
   }
   
-  //*****************EDITS FROM HERE
+  /********************************************************************************
+    * Private. Resets aimAtX and aimAtY to the next coordinate pair to be shot at.
+    * Called by takeAShot(). Decides according to direction what the next coordinates
+    * should be
+    *********************************************************************************/
   private void setNextShot() { //resets aimAtX and aimAtY to next coordinate to be shot at
     System.out.println("setNextShot(): aimAtX = " + aimAtX + ", aimAtY = " + aimAtY);
     System.out.println("setNextShot(): direction = " + direction);
@@ -117,10 +142,15 @@ public class ComputerPlayer extends Player {
     } 
   }
   
-  //think through logic.
+/********************************************************************************
+  * Decides whether to take a random shot or to shoot at (aimAtX, aimAtY). This 
+  * decision is dependent on lastShotResult and wasRandomShot. Calls on setNextShot().
+  * Shoots at the Player "other". Returns result of shot.
+  * 
+  * @param      other       Player at whom the computer is shooting
+  * @return     int      whether the shot was a miss, hit, and if the boat sunk
+  *********************************************************************************/
   public int takeAShot (Player other) {
-    round++;
-    System.out.println("\n//////////////////////////////// ROUND " + round + " ///////////////////////////////");
     System.out.println("takeAShot(): lastShotResult = " + lastShotResult + 
                        "\twasRandomShot = "+ wasRandomShot);
     if ((lastShotResult == MISS && wasRandomShot == true) || lastShotResult == HIT_AND_SUNK) {
@@ -166,6 +196,16 @@ public class ComputerPlayer extends Player {
     return lastShotResult;
   }
   
+  /********************************************************************************
+    * Private. Returns whether or not the ComputerPlayer must go backwards and start
+    * guessing again. Returns true if player should go back: (i) they have hit a wall
+    * or (ii) they have already guessed (x,y).
+    * 
+    * @param      p      Player being shot at/ComputerPlayer's opponent.
+    * @param      x      current x-coordinate of pair that is checked
+    * @param      y      current y-coordinate of pair that is checked
+    * @return     boolean       whether or not the ComputerPlayer should go back.
+    *********************************************************************************/ 
   private boolean goBack(Player p, int x, int y) {
     boolean shouldTurnAround = false;
     int newUp = y - 1;
@@ -176,16 +216,14 @@ public class ComputerPlayer extends Player {
     if (newUp < GRID_DIMENSIONS || newDown > GRID_DIMENSIONS
           || newLeft < GRID_DIMENSIONS || newRight > GRID_DIMENSIONS) { //if next coord is off the grid
       shouldTurnAround = true;
-    } else if (alreadyHit(p, x, newUp) || alreadyHit(p, x, newDown)
-                 || alreadyHit(p, newLeft, y) || alreadyHit(p, newRight, y)) { //if next coord has already been hit
+    } else if (previouslyShotAt(x, newUp) || previouslyShotAt(x, newDown)
+                 || previouslyShotAt(newLeft, y) || previouslyShotAt(newRight, y)) { //if next coord has already been hit
       shouldTurnAround = true;
     }
     return shouldTurnAround;
   }
-  
-  private boolean alreadyHit(Player p, int x, int y) {
-    return p.grid[x][y].getShotAt();
-  }
+
+  ///COMMENT HERE
   
   private void findOtherBoatEnd(Player p) {
     int line;
@@ -209,10 +247,19 @@ public class ComputerPlayer extends Player {
       }
   }
   
+  /********************************************************************************
+    * Shoots at Player p, with the input coordinates. Sets lastShotResult according to
+    * output of p.gotShot(x,y). 
+    * 
+    * @param     p     Player to be shot at
+    * @param     x       x-coordinate to shoot at
+    * @param     y       y-coordinate to shoot at
+    *********************************************************************************/
   public void shoot (Player p, int x, int y) throws InvalidShotException {
     System.out.println("shoot(P, x, y): (" + aimAtX + ", " + aimAtY + ").");
     try {
       lastShotResult = p.gotShot(x, y); //uses MISS (0), HIT_NOT_SUNK (1) and HIT_AND_SUNK (2)
+      shotSoFarGrid[x-1][y-1].setShotAt(true);
     } catch (InvalidShotException e) {
       System.out.println("shoot(p, x, y): Exception caught");
       System.out.println("Old aimAtX: " + aimAtX + "\taimAtY: " + aimAtY);
@@ -222,7 +269,6 @@ public class ComputerPlayer extends Player {
         aimAtX = (direction == LEFT) ? aimAtX-- : aimAtY++ ;
       }
       if (goBack(p, aimAtX, aimAtY)) {
-        //STUCK HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEREEEEE
         System.out.println("@@@@@@@@@@@@@@@Re-doing shoot(p, x, y)");
         shoot(p, aimAtX, aimAtY);
         System.out.println("New aimAtX: " + aimAtX + "\taimAtY: " + aimAtY);
@@ -233,16 +279,24 @@ public class ComputerPlayer extends Player {
 //    System.out.println("Computer shooting");
   }
   
+  /********************************************************************************
+    * Picks a random point to shoot at, and then calls on shoot(Player p, int x, int y).
+    * 
+    * @param      p     Player to shoot at.
+    *********************************************************************************/
   public void shoot(Player p) throws InvalidShotException {
     System.out.println("shoot(P):");
     pickAPoint(); 
     System.out.println("Point: (" + aimAtX + ", " + aimAtY + ").");
     shoot(p, aimAtX, aimAtY); //aimAtX and aimAtY is set in pickAPoint()
   }
-  
-  //**************** EDITS END HERE
-  
-  
+
+   /******************************************************************************
+    * Places all the ComputerPlayer's boats randomly. Chooses an orientation (vertical/
+    * horizontal) and then a direction, and then sets the first coordinate randomly
+    * and calculates the last coordinate according to direction. 
+    * Sets each boat's start and end coordinates, and then sets the cells themselves.
+    *******************************************************************************/  
  public void placeBoats() { // != placeBoat() from Player
     for (int i = 0; i < getNumBoats(); i++) {
       //System.out.println("\n/////////////////////////placeBoats() round " + (i+1) + " //////////////////////////////////");
@@ -371,11 +425,24 @@ public class ComputerPlayer extends Player {
       setCoords(ship, direction); //finding new starting coordinates
     }
   }
-  
+   /******************************************************************************
+    * Private. Returns true if input is within grid dimensions.
+    * 
+    * @param     coord     coordinate value we're checking
+    * @return     boolean      returns true if the coordinate is valid.
+    *******************************************************************************/
   private boolean isValidCoord (int coord) {
     return (coord > 0  && coord <= GRID_DIMENSIONS);
   }
   
+   /******************************************************************************
+    * Private. Returns true if current boat is overlapping another, already-placed 
+    * boat. 
+    * 
+    * @param     blackPearl     Boat the ComputerPlayer is trying to place.
+    * @param     direction     Boat's direction (UP/DOWN/LEFT/RIGHT).
+    * @return     boolean      returns true if the boat is overlapping another.
+    *******************************************************************************/
   private boolean doesBoatOverlap(Boat blackPearl, int direction) { //overrides Player's doesBoatOverlap
     boolean overlap = false;
     if (blackPearl.getStartX()==blackPearl.getEndX()) {
@@ -400,26 +467,52 @@ public class ComputerPlayer extends Player {
     return overlap;
   }
   
+   /******************************************************************************
+    * Returns a random boat orientation.
+    * 
+    * @return     int    boatOrientation (0 or 1)
+    *******************************************************************************/
   private int getBoatOrientation() { //0 = vertical, 1 = horizontal
     return (int)Math.round(Math.random());
   }
   
+  /******************************************************************************
+    * Returns an int for vertical boat direction.
+    * 
+    * @return     int    boat direction (UP or DOWN)
+    *******************************************************************************/
   private int getUpOrDown() { //2 = up, down = 3
     return (int)Math.round(Math.random() + 2);
   }
   
+  /******************************************************************************
+    * Returns an int for horizontal boat direction.
+    * 
+    * @return     int    boat direction (LEFT or RIGHT)
+    *******************************************************************************/
   private int getLeftOrRight() { //4 = left, 5 = right
     return (int)Math.round(Math.random() + 4);
   }
   
+  /******************************************************************************
+    * Returns aimAtX.
+    * 
+    * @return     int    aimAtX.
+    *******************************************************************************/
   public int getAimAtX() {
     return aimAtX;
   }
   
+   /******************************************************************************
+    * Returns aimAtY.
+    * 
+    * @return     int    aimAtY.
+    *******************************************************************************/
   public int getAimAtY() {
     return aimAtY;
   }
-  
+
+  //testing main
   public static void main (String[] args) {
     ComputerPlayer c = new ComputerPlayer();
     c.placeBoats();
